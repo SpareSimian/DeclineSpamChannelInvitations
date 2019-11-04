10688690262f6ff5dcf32a1800668702326fff2f
Kenneth Porter <shiva+git@sewingwitch.com>
2019-11-04 14:22:49 -0800

    More general URL pattern. Proposed auto-popup of player reporting dialog.

--- DeclineSpamChannelInvitations.lua
+++ DeclineSpamChannelInvitations.lua
@@ -5,9 +5,11 @@ local dsciFrame = CreateFrame("Frame")
 dsciFrame:SetScript("OnEvent", function(self, event, channelID, name)
       if ("CHANNEL_INVITE_REQUEST" == event) then
          print("DeclineSpamChannelInvitations:", self, event, channelID, name)
-         if string.find(channelID, "GOLD") then
+         if string.find(channelID, "www[A-Z]+com") then
             print("Spammer " .. name .. "invited you to channel " .. channelID)
             DeclineChannelInvite(channelID)
+            -- we could open the report dialog but we need a PlayerLocation, how do we get that?
+            -- C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_SPAM, name, playerLocation);
             return
          end
          -- for all other channels, invoke default handler
