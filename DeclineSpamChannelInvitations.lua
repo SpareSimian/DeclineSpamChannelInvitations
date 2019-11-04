626d0b7f77602c55e0af5341a58de4f6369e59dd
Kenneth Porter <shiva+git@sewingwitch.com>
2019-11-03 22:21:05 -0800

    Actually install the handler. Check that the event is the one we registered for.

--- DeclineSpamChannelInvitations.lua
+++ DeclineSpamChannelInvitations.lua
@@ -1,14 +1,22 @@
 -- DeclineSpamChannelInvitations
 
-local dsciFrame = CreateFrame("DeclineSpamChannelInvitations")
+local dsciFrame = CreateFrame("Frame")
 
-function dsciFrame:OnChannelInviteRequest(...)
-   print("DeclineSpamChannelInvitations:", ...)
-
-   -- todo: match spammer's channel pattern and invoke DeclineChannelInvite()
-   -- for all other channels, invoke UIParent_OnEvent()
-end
+dsciFrame:SetScript("OnEvent", function(self, event, channelID, name)
+      if ("CHANNEL_INVITE_REQUEST" == event) then
+         print("DeclineSpamChannelInvitations:", self, event, channelID, name)
+         if string.find(channelID, "GOLD") then
+            print("Spammer " .. name .. "invited you to channel " .. channelID)
+            DeclineChannelInvite(channelID)
+            return
+         end
+         -- for all other channels, invoke default handler
+         UIParent_OnEvent(UIParent, event, channelID, name)
+      end
+   end
+)
 
 dsciFrame:RegisterEvent("CHANNEL_INVITE_REQUEST")
 
--- UIParent::UnregisterEvent("")
+-- uncomment this once we know above is working
+-- UIParent::UnregisterEvent("CHANNEL_INVITE_REQUEST")
