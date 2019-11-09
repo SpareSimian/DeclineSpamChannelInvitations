e2d235d1ae24ef07cf4b6cff35590e056834513e
Kenneth Porter <shiva+git@sewingwitch.com>
2019-11-08 20:33:32 -0800

    Forgot a space.

--- DeclineSpamChannelInvitations.lua
+++ DeclineSpamChannelInvitations.lua
@@ -6,7 +6,7 @@ dsciFrame:SetScript("OnEvent", function(self, event, channelID, name)
       if ("CHANNEL_INVITE_REQUEST" == event) then
          print("DeclineSpamChannelInvitations:", self, event, channelID, name)
          if string.find(channelID, "www[A-Z0-9]+com") then
-            print("Spammer " .. name .. "invited you to channel " .. channelID)
+            print("Spammer " .. name .. " invited you to channel " .. channelID)
             DeclineChannelInvite(channelID)
             -- we could open the report dialog but we need a PlayerLocation, how do we get that?
             -- C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_SPAM, name, playerLocation);
